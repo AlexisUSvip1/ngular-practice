2f07f208bc731e5b25d4d9dfd0c3968ad16efc77
AlexisUSvip1 <aultreras@grupoabraxas.com>
2025-06-17 14:18:35 -0600

    nuevas implementaciones

--- src/app/components/movies-profile/movies-profile.ts
+++ src/app/components/movies-profile/movies-profile.ts
@@ -1,36 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FetchApiMovies } from '../../../utils/fetch.components'; // ajusta si es necesario
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
+import { FetchApiMovies } from '../../../utils/fetch.components';
 
 @Component({
   selector: 'app-movies-profile',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './movies-profile.html',
-  styleUrl: './movies-profile.css'
+  styleUrl: './movies-profile.css',
 })
 export class MoviesProfile implements OnInit {
   movieId: string | null = null;
   movie: any = null;
+  favoriteMovies: any[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private movieService: FetchApiMovies
-  ) {}
+    private movieService: FetchApiMovies,
+    private cd: ChangeDetectorRef,
+    private library: FaIconLibrary
+  ) {
+    this.library.addIcons(faSolidHeart, faRegularHeart);
+  }
 
   ngOnInit(): void {
     this.movieId = this.route.snapshot.paramMap.get('id');
-
+    this.loadFavoriteMovies();
     if (this.movieId) {
       this.getMovieDetails();
     }
   }
 
+  loadFavoriteMovies(): void {
+    const storedFavorites = localStorage.getItem('favoriteMovies');
+    this.favoriteMovies = storedFavorites ? JSON.parse(storedFavorites) : [];
+  }
+
+  isFavorite(movieId: number): boolean {
+    return this.favoriteMovies.some(movie => movie.id === movieId);
+  }
+
   getMovieDetails(): void {
     this.movieService.getMoviesIdi(`/movie/${this.movieId}`).subscribe((data) => {
       this.movie = data;
+      this.cd.detectChanges();
     });
   }
 }
--- src/app/components/movies/movies.ts
+++ src/app/components/movies/movies.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FetchApiMovies } from '../../../utils/fetch.components';
 import { CommonModule } from '@angular/common';
 import { Pagination } from "../pagination/pagination";
@@ -6,6 +6,10 @@ import { NgxSpinnerModule, NgxSpinnerService } from "ngx-spinner";
 import { Search } from "./search/search";
 import { MoviesProfile } from '../movies-profile/movies-profile';
 import { Router } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
+
 @Component({
   selector: 'app-movies',
   standalone: true,
@@ -14,7 +18,7 @@ import { Router } from '@angular/router';
     Pagination,
     NgxSpinnerModule,
     Search,
-    MoviesProfile
+    FontAwesomeModule,
 ],
   templateUrl: './movies.html',
   styleUrl: './movies.css',
@@ -22,28 +26,35 @@ import { Router } from '@angular/router';
 export class Movies implements OnInit {
   
   movies:any[] = [] 
+  titleMovies: string = ""
   loadedImages: Set<number> = new Set()
   currentPage = 1;
   totalPages = 0;
   searchQuery: string =""
   category: string ="popular"
   activeCategory: string = ""
+  favoriteMovies: any[] = [];
   constructor(
     private movieService: FetchApiMovies,
     private spinner: NgxSpinnerService,
-    private router: Router
+    private router: Router,
+    private cd: ChangeDetectorRef,
+    private library: FaIconLibrary,
+
   ) {
     this.movieService = movieService;
     this.spinner= spinner
     this.router=router
+    this.cd = cd
+    this.library.addIcons(faSolidHeart, faRegularHeart);
+    
   }
 
   ngOnInit(): void {
     this.spinner.show();
     this.getMoviesPopulars();
     this.category
-    console.log(this.category)
-
+    this.loadFavoriteMovies();
   }
 
   getMoviesPopulars (page: number = 1, query?: string): void {
@@ -52,13 +63,15 @@ export class Movies implements OnInit {
       this.currentPage = response.page;
       this.totalPages = response.total_pages;
       this.spinner.hide()
+      this.cd.detectChanges();
     });
   }
-  changeCategory (categotyMovie: string) {
-    this.category=categotyMovie
-    this.activeCategory=categotyMovie
+  changeCategory(categotyMovie: string): void {
+    this.category = categotyMovie;
+    this.activeCategory = categotyMovie;
+    this.switchTitle(this.category);
     this.spinner.show();
-    this.getMoviesPopulars(1)
+    this.getMoviesPopulars(1);
   }
 
   onSearch(query: string): void {
@@ -90,5 +103,48 @@ export class Movies implements OnInit {
   }
   verDetalle(movieId: number): void {
     this.router.navigate(['/detalle', movieId]);
+  }
+  favoriteMovie(movieId: number): void {
+    const movieIndex = this.favoriteMovies.findIndex(movie => movie.id === movieId);
+    
+    if (movieIndex > -1) {
+      this.favoriteMovies.splice(movieIndex, 1);
+    } else {
+      const movie = this.movies.find(m => m.id === movieId);
+      if (movie) {
+        this.favoriteMovies.push(movie);
+      }
+    }
+
+    this.saveFavoriteMovies();
+  }
+  isFavorite(movieId: number): boolean {
+    return this.favoriteMovies.some(fav => fav.id === movieId);
+  }
+  loadFavoriteMovies(): void {
+    const storedFavorites = localStorage.getItem('favoriteMovies');
+    this.favoriteMovies = storedFavorites ? JSON.parse(storedFavorites) : [];
+  }
+  saveFavoriteMovies(): void {
+    localStorage.setItem('favoriteMovies', JSON.stringify(this.favoriteMovies));
+  }
+  switchTitle(title: string): void {
+    switch (title) {
+      case 'popular':
+        this.titleMovies = 'Películas Populares';
+        break;
+      case 'top_rated':     
+        this.titleMovies = 'Películas Mejor Valoradas';
+        break;
+      case 'now_playing':
+        this.titleMovies = 'Películas en Cartelera';
+        break;
+      case 'upcoming':
+        this.titleMovies = 'Próximamente';
+        break;
+      default:
+        this.titleMovies = 'Películas';
+        break;  
+    }
   }
 }
